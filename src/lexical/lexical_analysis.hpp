c53e30fad0b93fb610c3a6ba29936952dcde019f
Shio3001 <taku2000maru@icloud.com>
2023-12-14 08:51:43 +0900

    字句解析の移植

--- src/lexical/lexical_analysis.hpp
+++ src/lexical/lexical_analysis.hpp
@@ -7,67 +7,20 @@
 #include "./../debug.hpp"
 #include "./../symbol.hpp"
 
-vstring getTokenString(char **token_string, int *token_progression)
+string getToken(char *source_text, int token_search_len)
 {
-    vstring token_string_vector;
-    for (int i = 0; i < token_progression_arr_size; i++)
-    {
-        int p = token_progression[i];
-
-        if (p == -1)
-        {
-            break;
-        }
-
-        char *token = token_string[p];
-        string token_string = string(token);
-        token_string_vector.push_back(token_string);
-    }
-    token_string_vector.push_back(DOLLAR);
-    return token_string_vector;
-}
-
-int getToken(char *source_text, char **token_string, int *token_progression, int token_search_len, int &token_string_endline, int &token_progression_endline)
-{
-
-    for (int i = 0; i < token_string_endline; i++)
-    {
-        // printf("token_search_len : %s %d\n", token_string[i], token_search_len);
-        bool result = strncmp(source_text, token_string[i], token_search_len) == 0;
-        // 存在するか否か
-
-        if (result)
-        {
-            token_progression[token_progression_endline] = i;
-            token_progression_endline++;
-            return i;
-        }
-    }
-
-    // printf("%s", source_code_current);
-
     // ここから登録
     char *new_token = (char *)calloc(code_token_len, sizeof(char));
     strncpy(new_token, source_text, token_search_len);
-    token_string[token_string_endline] = new_token;
+    string token_string = string(new_token);
 
-    token_progression[token_progression_endline] = token_string_endline;
-    token_string_endline++;
-    token_progression_endline++;
-    return token_string_endline;
+    return token_string;
 }
 // スキャナ
-void lexSyntax(char *source_code, char **token_string, int *token_progression)
+vector<string> lexSyntax(char *source_code)
 {
     // source_codeとtokenのポインタを得る
-
-    int token_string_endline = 0;
-    int token_progression_endline = 0;
-
-    for (int i = 0; i < token_progression_arr_size; i++)
-    {
-        token_progression[i] = -1;
-    }
+    vector<string> tokens;
 
     int i_s = 0; // 現在地点
     for (;;)
@@ -111,10 +64,13 @@ void lexSyntax(char *source_code, char **token_string, int *token_progression)
             exit(1);
         }
         printf("token_search_len %d\n", token_search_len);
-        int current_token_id = getToken(&source_code[i_s], token_string, token_progression, token_search_len, token_string_endline, token_progression_endline);
+        string current_token = getToken(&source_code[i_s], token_search_len);
+        tokens.push_back(current_token);
         // printf("current_token_id : %d", current_token_id);
         i_s += token_search_len;
     }
+    tokens.push_back(DOLLAR);
+    return tokens;
 
     // output_token_string(token_string, token_string_arr_size);
     // output_token_progression(token_string, token_progression, token_progression_endline);
